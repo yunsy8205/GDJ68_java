73d3d400a40e0588bf8c2f7f389c4f5fa6b80daa
yunsy8205 <yunsy8205@naver.com>
2023-06-27 15:54:58 +0900

    study

---
+++ src/com/iu/study2/Study3.java
@@ -0,0 +1,23 @@
+package com.iu.study2;
+
+public class Study3 {
+
+	public static void main(String[] args) {
+		// if, if-else, if -elseif-else
+		// switch() case
+		int num=5;
+		
+		switch(num*2) {
+		case 10:
+			System.out.println("num = 10");
+		         //10과 같으면 아래 내용을 실행한다.
+		case 20:
+			System.out.println("num = 20");
+			
+		default: // 그외 나머지
+			System.out.println("num 모름");
+		}
+	
+	}
+
+}
